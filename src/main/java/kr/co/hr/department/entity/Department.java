f166e6b019d6389c70c7305264a84749ca5384e7
s4ngg <s4ngg@naver.com>
2026-03-20 18:12:58 +0900

    feat(department) : Department Entity 생성

--- src/main/java/kr/co/hr/department/entity/Department.java
+++ src/main/java/kr/co/hr/department/entity/Department.java
@@ -1,5 +1,36 @@
 package kr.co.hr.department.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long departmentId;
+
+    private String deptCode;
+    private String deptName;
+    private Long managerId;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
