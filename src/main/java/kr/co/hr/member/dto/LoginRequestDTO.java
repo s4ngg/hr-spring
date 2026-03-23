9838ac8272040aaa1ca7da41faf57c5f6e81db69
tig <leehjpp4@gmail.com>
2026-03-23 11:01:35 +0900

    feat: member.dto 패키지 생성 및 DTO 기능 추가
    - LoginRequestDTO 기능 추가

---
+++ src/main/java/kr/co/hr/member/dto/LoginRequestDTO.java
@@ -0,0 +1,11 @@
+package kr.co.hr.member.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDTO {
+	private String loginId;
+	private String password;
+}
