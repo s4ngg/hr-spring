532d5408d15cfb5e79e70c3592f4b460e1086ac2
s4ngg <s4ngg@naver.com>
2026-03-23 10:43:38 +0900

    feat(department) : Member 연관관계 추가

--- src/main/java/kr/co/hr/department/entity/Department.java
+++ src/main/java/kr/co/hr/department/entity/Department.java
@@ -1,11 +1,21 @@
 package kr.co.hr.department.entity;
 
+<<<<<<< Updated upstream
 import java.time.LocalDateTime;
 
+=======
+import kr.co.hr.member.entity.Member;
+import java.time.LocalDateTime;
+>>>>>>> Stashed changes
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+<<<<<<< Updated upstream
+=======
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+>>>>>>> Stashed changes
 import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,9 +33,19 @@ public class Department {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long departmentId;
 
+<<<<<<< Updated upstream
     private String deptCode;
     private String deptName;
     private Long managerId;
+=======
+    @ManyToOne
+    @JoinColumn(name = "manager_id")
+    private Member manager;
+    
+    private String deptCode;
+    private String deptName;
+    
+>>>>>>> Stashed changes
 
     private LocalDateTime createdAt;
 
