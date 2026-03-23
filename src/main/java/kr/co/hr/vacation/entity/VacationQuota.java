4f1ee70964e1f60b3f73cd977970780692b77c2a
tig <leehjpp4@gmail.com>
2026-03-23 15:56:39 +0900

    fix<VacationQuota> : member 컬럼에 한 직원이 연도별로 여러 할당 가능하도록 @ManyToOne 추가

--- src/main/java/kr/co/hr/vacation/entity/VacationQuota.java
+++ src/main/java/kr/co/hr/vacation/entity/VacationQuota.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import kr.co.hr.member.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,11 +18,15 @@ import lombok.Setter;
 @AllArgsConstructor
 public class VacationQuota {
 	
-    @OneToOne 
-    @JoinColumn(name = "member_id")
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long quotaId; // PK - @Id 추가!
+	
+	// 한 직원이 연도별로 여러 할당 가능 → ManyToOne
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
     
-    private Integer quotaId;
     private Integer year;
     private Integer totalDays; 
     private Integer usedDays;  
