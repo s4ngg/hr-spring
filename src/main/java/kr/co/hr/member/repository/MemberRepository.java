8eeadd1ec62c18052aed863b7a0a92d2bcafc178
tig <leehjpp4@gmail.com>
2026-03-23 10:33:28 +0900

    feat: MemberReopsitory 기능을 추가

--- src/main/java/kr/co/hr/member/repository/MemberRepository.java
+++ src/main/java/kr/co/hr/member/repository/MemberRepository.java
@@ -1,9 +1,13 @@
 package kr.co.hr.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.co.hr.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long>{
-
+	
+	Optional<Member> findByEmailOrEmployeeNo(String Email, String employeeNo);
+	
 }
