de38ad69c94b81f323e6e0750a5a70c0ae49e0ac
tig <leehjpp4@gmail.com>
2026-03-23 13:58:24 +0900

    feat<VacationResponseDTO>: DTO 기능 추가

---
+++ src/main/java/kr/co/hr/vacation/dto/VacationResponseDTO.java
@@ -0,0 +1,41 @@
+package kr.co.hr.vacation.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(description = "휴가 내역 응답 정보")
+public class VacationResponseDTO {
+	
+	@Schema(description = "휴가신청 고유 번호", example = "101")
+	private Long vacationId; 		// 수정을 대비한 고유 ID
+	
+	@Schema(description = "신청자 이름", example = "홍길동")
+	private String memberName; 		// 신청자 이름
+	
+	@Schema(description = "휴가 종류", example = "연차,병가,육아휴직")
+	private String vacationType; 	//휴가 종류
+	
+	@Schema(description = "휴가 시작일", example = "2026-03-23")
+	private LocalDate startDate; 	//시작일
+	
+	@Schema(description = "휴가 종료일", example = "2026-03-30")
+	private LocalDate endDate; 		//종료일 
+	
+	@Schema(description = "휴가 일수", example = "8")
+	private Integer days; 			//휴가일수
+	
+	@Schema(description = "휴가 상태", example = "보류중,진행중,거절")
+	private String status;			//휴가상태
+	
+	@Schema(description = "신청 일시", example = "2026-03-21")
+	private LocalDateTime createdAt; // 신청일
+	
+	@Schema(description = "사유", example = "생일파티")
+	private String reason; 			// 사유
+}
