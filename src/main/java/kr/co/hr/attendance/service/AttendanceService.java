62f60cdead4ad044b9cda0c4bcba9bf05adbabb1
Hello-pythworld <yeonghun4051@daum.net>
2026-03-24 09:50:40 +0900

    feat: 근태관리 CRUD 구현

--- src/main/java/kr/co/hr/attendance/service/AttendanceService.java
+++ src/main/java/kr/co/hr/attendance/service/AttendanceService.java
@@ -2,13 +2,20 @@ package kr.co.hr.attendance.service;
 
 import java.util.List;
 
+import kr.co.hr.attendance.dto.AttendanceRequestDTO;
 import kr.co.hr.attendance.dto.AttendanceResponseDTO;
 
 public interface AttendanceService {
 	
 	List<AttendanceResponseDTO> getAllAttendances();
 	
-	List<AttendanceResponseDTO> getAttendanceByMember(Long memberId);
+	AttendanceResponseDTO checkIn(AttendanceRequestDTO requestDTO);
+	
+	AttendanceResponseDTO checkOut(Long attendanceId, AttendanceRequestDTO requestDTO);
+
+	void deleteAttendance(Long attendanceId);
+	
+	List<AttendanceResponseDTO> getAttendancesByMember(Long memberId);
 }
 
 	
