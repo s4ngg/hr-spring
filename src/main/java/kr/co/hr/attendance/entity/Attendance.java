e7689b52ef06a25a7352c96f3fb8bc8923ce5289
Hello-pythworld <yeonghun4051@daum.net>
2026-03-24 13:53:19 +0900

    비밀번호유효성검사 및 notblank어노테이션추가

--- src/main/java/kr/co/hr/attendance/entity/Attendance.java
+++ src/main/java/kr/co/hr/attendance/entity/Attendance.java
@@ -11,17 +11,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
-
 import kr.co.hr.member.entity.Member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "attendance")
 @Getter
-@Setter
 @NoArgsConstructor
 public class Attendance {
 
@@ -46,4 +44,18 @@ public class Attendance {
 
     private String status;
 
+    
+ // ✅ 출근용 메서드
+    @PrePersist
+    public void prePersist() {
+        this.workDate = LocalDate.now();
+        this.checkIn = LocalTime.now();
+        this.status = "출근";
+    }
+    
+ // ✅ 퇴근용 메서드
+    public void checkOut() {
+        this.checkOut = LocalTime.now();
+        this.status = "퇴근";
+    }
 }
