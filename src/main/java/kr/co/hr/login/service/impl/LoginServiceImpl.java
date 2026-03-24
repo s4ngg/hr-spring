0d943b0274834e284ade1e486bfdb16638205bb7
tig <leehjpp4@gmail.com>
2026-03-24 16:19:14 +0900

    feat<LoginServiceImpl> : 로그인 기능 구현
    - 이메일과 사번으로 통합 검색 로직
    - 비밀번호 검증 로직

---
+++ src/main/java/kr/co/hr/login/service/impl/LoginServiceImpl.java
@@ -0,0 +1,34 @@
+package kr.co.hr.login.service.impl;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import kr.co.hr.login.dto.LoginRequestDTO;
+import kr.co.hr.login.service.LoginService;
+import kr.co.hr.member.entity.Member;
+import kr.co.hr.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class LoginServiceImpl implements LoginService{
+
+	private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    
+    
+    @Override
+    public String authenticate(LoginRequestDTO dto) {
+        // 1. 이메일 또는 사번 으로 통합 검색
+        Member member = memberRepository.findByEmailOrEmployeeNo(dto.getLoginId(), dto.getLoginId())
+                .orElseThrow(() -> new RuntimeException("사번 또는 이메일이 존재하지 않습니다."));
+
+        // 2. 비밀번호 검증 
+        if (!passwordEncoder.matches(dto.getPassword(), member.getPassword())) {
+            throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return member.getName() + "님, 환영합니다! (사번: " + member.getEmployeeNo() + ")";
+    }
+}
+
