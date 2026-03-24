c0b08910d34747b359ca22b4c04f4347bdd30aec
tig <leehjpp4@gmail.com>
2026-03-24 09:55:24 +0900

    fix<VacationServiceImpl>: 휴가 신청 로직부분 수정

--- src/main/java/kr/co/hr/member/repository/MemberRepository.java
+++ src/main/java/kr/co/hr/member/repository/MemberRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.co.hr.member.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long>{
-
+public interface MemberRepository extends JpaRepository<Member, Long>{	
+	
 }
