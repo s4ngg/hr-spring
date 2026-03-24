aa5c4f594f279f73954931a800a849d42ae390f9
tig <leehjpp4@gmail.com>
2026-03-24 14:43:06 +0900

    feat<MemberRepository> 이메일로도 찾는 메서드 추가

--- src/main/java/kr/co/hr/member/repository/MemberRepository.java
+++ src/main/java/kr/co/hr/member/repository/MemberRepository.java
@@ -8,6 +8,9 @@ import kr.co.hr.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long>{
 	
+	
+	Optional<Member> findByEmail(String email);
+	
 	Optional<Member> findByEmailOrEmployeeNo(String Email, String employeeNo);
 
 	
