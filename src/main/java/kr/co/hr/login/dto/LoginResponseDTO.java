973f8975e93acd13e356507e12ba8c054de512f9
tig <leehjpp4@gmail.com>
2026-03-25 15:27:27 +0900

    refactor<DTOs> @Schema 어노테이션을 추가했습니다

--- src/main/java/kr/co/hr/login/dto/LoginResponseDTO.java
+++ src/main/java/kr/co/hr/login/dto/LoginResponseDTO.java
@@ -1,14 +1,28 @@
 package kr.co.hr.login.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
+
+
+@Schema(description = "로그인 응답 요청 ")
 @Getter
 @Builder
 public class LoginResponseDTO {
+	
+	@Schema(description = "회원 번호", example = "1")
 	private Long memberId;
+	
+	@Schema(description = "사번 ", example = "101")
 	private String employeeNo;
+	
+	@Schema(description = "이름", example = "병가,연차")
 	private String name;
+	
+	@Schema(description = "직책", example = "병가,연차")
 	private String role;
-	private String deptName;
+	
+	@Schema(description = "부서 번호", example = "15")
+	private String departmentId;
 }
