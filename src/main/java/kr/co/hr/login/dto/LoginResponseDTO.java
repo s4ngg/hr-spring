c8a5ec3750b7fc89bd69f0d0eda5502a315afea5
tig <leehjpp4@gmail.com>
2026-03-25 15:41:59 +0900

    refactor<LoginResponseDTO> 부서명과 토큰값 추가

--- src/main/java/kr/co/hr/login/dto/LoginResponseDTO.java
+++ src/main/java/kr/co/hr/login/dto/LoginResponseDTO.java
@@ -14,15 +14,21 @@ public class LoginResponseDTO {
 	@Schema(description = "회원 번호", example = "1")
 	private Long memberId;
 	
-	@Schema(description = "사번 ", example = "101")
+	@Schema(description = "사번 ", example = "20260101")
 	private String employeeNo;
 	
-	@Schema(description = "이름", example = "병가,연차")
+	@Schema(description = "이름", example = "이현재")
 	private String name;
 	
-	@Schema(description = "직책", example = "병가,연차")
+	@Schema(description = "직책", example = "MANAGER")
 	private String role;
 	
+	@Schema(description = "부서명", example = "개발팀")
+    private String deptName; 
+	
 	@Schema(description = "부서 번호", example = "15")
-	private String departmentId;
+	private Long departmentId;
+	
+	@Schema(description = "JWT 토큰")
+    private String token; // 토큰도 잊지 말고 꼭 담아주세요.
 }
