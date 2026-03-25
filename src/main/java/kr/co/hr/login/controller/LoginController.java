b7566d6c21ee345a74c87e642aa00b9f225c3aff
tig <leehjpp4@gmail.com>
2026-03-25 16:07:34 +0900

    refactor<LoginServiceImpl> authenticate 메서드 수정
    - 기존 JWT 문자열 반한만 하던 로직을 DTO 기반 응답 구조로 변경
    
    - 흥답 확장성 및 프론트 연동 가능하게

--- src/main/java/kr/co/hr/login/controller/LoginController.java
+++ src/main/java/kr/co/hr/login/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.co.hr.login.dto.LoginRequestDTO;
+import kr.co.hr.login.dto.LoginResponseDTO;
 import kr.co.hr.login.service.LoginService;
 import lombok.RequiredArgsConstructor;
 
@@ -21,9 +22,9 @@ public class LoginController {
 	
 	@Operation(summary = "로그인", description = "사용자의 사번 or 이메일 비밀번호를 받아 로그인을 진행하고 성공시 JWT 토큰 반환.")
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestBody LoginRequestDTO dto) {
+	public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO dto) {
 	    // 1. 서비스에서 토큰을 받아옵니다.
-	    String token = loginService.authenticate(dto);
+	    LoginResponseDTO token = loginService.authenticate(dto);
 	    
 	    return ResponseEntity.ok(token);
 }
