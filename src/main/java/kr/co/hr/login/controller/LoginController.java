76a9b9665f66d20c8d751751348661050dab7cc8
tig <leehjpp4@gmail.com>
2026-03-25 10:40:30 +0900

    feat<Controller,RequestDTO> swagger 사용을 위한 tag 추가

--- src/main/java/kr/co/hr/login/controller/LoginController.java
+++ src/main/java/kr/co/hr/login/controller/LoginController.java
@@ -5,16 +5,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.co.hr.login.dto.LoginRequestDTO;
 import kr.co.hr.login.service.LoginService;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "Login", description = "로그인 관련 API")
 @RestController
 @RequestMapping("/api") 
 @RequiredArgsConstructor
 public class LoginController {
 	private final LoginService loginService;
 	
+	@Operation(summary = "로그인", description = "사용자의 사번 or 이메일 비밀번호를 받아 로그인을 진행하고 성공시 JWT 토큰 반환.")
 	@PostMapping("/login")
     public String login(@RequestBody LoginRequestDTO dto) {
         return loginService.authenticate(dto);
--- src/main/java/kr/co/hr/login/dto/LoginRequestDTO.java
+++ src/main/java/kr/co/hr/login/dto/LoginRequestDTO.java
@@ -1,10 +1,15 @@
 package kr.co.hr.login.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class LoginRequestDTO {
+	
+	@Schema(description = "사번 또는 이메일", example = "2026001")
 	private String loginId; // 사번 or 이메일 
+	
+	@Schema(description = "비밀번호", example = "1234")
     private String password;
 }
