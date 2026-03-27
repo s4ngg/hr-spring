bb070b914c09a1fe00261b4d51086b8abea2b5f2
Hello-pythworld <yeonghun4051@daum.net>
2026-03-27 09:57:57 +0900

    스프링 파라미터 문제 해결

--- src/main/java/kr/co/hr/global/config/SecurityConfig.java
+++ src/main/java/kr/co/hr/global/config/SecurityConfig.java
@@ -18,6 +18,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+        .formLogin(form -> form.disable())
+
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
