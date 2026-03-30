d1fa5e3ecb22a826abee4c6d2bd8d15f1c26a37a
s4ngg <s4ngg@naver.com>
2026-03-30 10:36:26 +0900

    docs: Itcontact 객체 관리 구조, 메일 실패 예외처리, 비동기 처리로 전환, 메일 구조 메서드 생성

--- src/main/java/kr/co/hr/itcontact/MailService.java
+++ src/main/java/kr/co/hr/itcontact/MailService.java
@@ -1,8 +1,8 @@
 package kr.co.hr.itcontact;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import kr.co.hr.itcontact.dto.ItContactRequestDto;
@@ -13,21 +13,36 @@ import lombok.RequiredArgsConstructor;
 public class MailService {
 
     private final JavaMailSender mailSender;
+    private final MailProperties mailProperties;
 
-    @Value("${it.team.email}")
-    private String itTeamEmail;
-
+    @Async
     public void sendItContactMail(ItContactRequestDto dto) {
-        SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setTo(itTeamEmail);
-        mail.setSubject("[IT 문의] " + dto.getSubject());
-        mail.setText(
-                "문의자: " + dto.getName() + "\n" +
-                "부서: " + dto.getDept() + "\n" +
-                "연락처: " + dto.getContact() + "\n" +
-                "이메일: " + dto.getEmail() + "\n\n" +
-                "내용:\n" + dto.getMessage()
+        try {
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(mailProperties.getEmail());
+            mail.setSubject("[IT 문의] " + dto.getSubject());
+            mail.setText(buildMailContent(dto));
+            mailSender.send(mail);
+        } catch (Exception e) {
+            throw new RuntimeException("메일 전송에 실패했습니다. 잠시 후 다시 시도해주세요.");
+        }
+    }
+
+    private String buildMailContent(ItContactRequestDto dto) {
+        return String.format("""
+                문의자: %s
+                부서: %s
+                연락처: %s
+                이메일: %s
+
+                내용:
+                %s
+                """,
+                dto.getName(),
+                dto.getDept(),
+                dto.getContact(),
+                dto.getEmail(),
+                dto.getMessage()
         );
-        mailSender.send(mail);
     }
 }
