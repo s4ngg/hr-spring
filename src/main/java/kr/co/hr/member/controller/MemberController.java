c7fb675c550ea68d7500ab7ba4bbe9c46e3a45e9
Hello-pythworld <yeonghun4051@daum.net>
2026-03-30 12:03:29 +0900

    작업 중 저장

--- src/main/java/kr/co/hr/member/controller/MemberController.java
+++ src/main/java/kr/co/hr/member/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -55,7 +56,7 @@ public class MemberController {
     }
 
     @Operation(summary = "직원 정보 수정")
-    @PutMapping("/{memberId}")
+    @PatchMapping("/{memberId}")
     public ResponseEntity<ApiResponse<MemberResponseDTO>> updateMember(
             @PathVariable("memberId") Long memberId,
             @Valid @RequestBody MemberRequestDTO requestDTO) {
