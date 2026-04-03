e15325e3adee28300e23dfcef164b1e61643b5f9
Hello-pythworld <yeonghun4051@daum.net>
2026-04-03 17:50:30 +0900

    Feat(Test): 배포 테스트 확인용 컨트롤러

---
+++ src/main/java/kr/co/hr/test/TestController.java
@@ -0,0 +1,30 @@
+package kr.co.hr.test;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import kr.co.hr.global.response.ApiResponse;
+import kr.co.hr.member.dto.MemberRequestDTO;
+import kr.co.hr.member.dto.MemberResponseDTO;
+import kr.co.hr.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+
+
+
+@RestController
+@RequestMapping("/api/members/test")
+@RequiredArgsConstructor
+public class TestController {
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<MemberResponseDTO>> createMember(
+            @Valid @RequestBody MemberRequestDTO requestDTO) {
+        return ApiResponse.success("직원 등록 성공", memberService.createMember(requestDTO));
+    }
+
+}
